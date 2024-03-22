645ff98274db48d1774237017d18971a34cbbacc
Amani454 <amani.soufi@univ-constantine.dz>
2024-03-21 14:46:36 -1100

    commit

---
+++ lib/chat.dart
@@ -0,0 +1,20 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class chat extends StatelessWidget {
+  const chat({super.key});
+   void signInOut(){
+    FirebaseAuth.instance.signOut();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(actions: [IconButton(onPressed:signInOut, icon: Icon(Icons.logout))],),
+      body: Center(
+        
+        child:Text("Welcom") ,
+      ),
+    );
+  }
+}
